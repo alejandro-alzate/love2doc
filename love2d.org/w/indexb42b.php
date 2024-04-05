d7761816dbe244e98623c2f284382ff7594595e6
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-04-05 18:45:45 -0500

    Automatic partial update At: Fri Apr  5 06:45:45 PM -05 2024

--- love2d.org/w/indexb42b.php
+++ love2d.org/w/indexb42b.php
@@ -20,7 +20,7 @@
 	xmlns:property="&property;">
 
 	<owl:Ontology rdf:about="http://love2d.org/wiki/Special:ExportRDF/Tutorial-3ANetworking_with_UDP-2DTheServer_-28日本語-29">
-		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-05T21:09:12+00:00</swivt:creationDate>
+		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-05T23:45:30+00:00</swivt:creationDate>
 		<owl:imports rdf:resource="http://semantic-mediawiki.org/swivt/1.0"/>
 	</owl:Ontology>
 	<swivt:Subject rdf:about="http://love2d.org/wiki/Special:URIResolver/Tutorial-3ANetworking_with_UDP-2DTheServer_-28日本語-29">
--- love2d.org/w/indexdbe1.php
+++ love2d.org/w/indexdbe1.php
@@ -20,7 +20,7 @@
 	xmlns:property="&property;">
 
 	<owl:Ontology rdf:about="http://love2d.org/wiki/Special:ExportRDF/Tutorial-3ANetworking_with_UDP-2DTheServer">
-		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-05T21:09:01+00:00</swivt:creationDate>
+		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-05T23:45:19+00:00</swivt:creationDate>
 		<owl:imports rdf:resource="http://semantic-mediawiki.org/swivt/1.0"/>
 	</owl:Ontology>
 	<swivt:Subject rdf:about="http://love2d.org/wiki/Special:URIResolver/Tutorial-3ANetworking_with_UDP-2DTheServer">
