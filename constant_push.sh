be4a101f9c04ecd0b3351cbde77b2c3db9d46365
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-04-05 17:01:07 -0500

    At: Fri Apr  5 05:01:07 PM -05 2024

--- constant_push.sh
+++ constant_push.sh
@@ -1,9 +1,24 @@
-MSG="Automatic partial update "
+MSG="Automatic partial update"
+COOLDOWN="3m"
 
 function upload() {
 	git push
 }
 
 function commit() {
-	git commit -
+	git commit -m "$(MSG) \
+	At: $(date) "
 }
+
+function add() {
+	git add *
+}
+
+function loop() {
+	add
+	commit
+	upload
+
+}
+
+loop
