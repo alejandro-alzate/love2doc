c3847fb52b8f6721510ab2e917f89b3360916726
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-04-05 18:25:42 -0500

    Automatic partial update At: Fri Apr  5 06:25:42 PM -05 2024

--- love2d.org/w/index37d4.php
+++ love2d.org/w/index37d4.php
@@ -20,7 +20,7 @@
 	xmlns:property="&property;">
 
 	<owl:Ontology rdf:about="http://love2d.org/wiki/Special:ExportRDF/Tutorial-3ACallback_Functions_-28Français-29">
-		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-05T18:51:31+00:00</swivt:creationDate>
+		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-05T23:25:40+00:00</swivt:creationDate>
 		<owl:imports rdf:resource="http://semantic-mediawiki.org/swivt/1.0"/>
 	</owl:Ontology>
 	<swivt:Subject rdf:about="http://love2d.org/wiki/Special:URIResolver/Tutorial-3ACallback_Functions_-28Français-29">
