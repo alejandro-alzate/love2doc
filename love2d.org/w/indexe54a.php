dabe07684773cb9033a4e6d6d56f98080356f5d1
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-04-05 18:18:20 -0500

    Automatic partial update At: Fri Apr  5 06:18:20 PM -05 2024

--- love2d.org/w/indexe54a.php
+++ love2d.org/w/indexe54a.php
@@ -20,7 +20,7 @@
 	xmlns:property="&property;">
 
 	<owl:Ontology rdf:about="http://love2d.org/wiki/Special:ExportRDF/In-2DMemory_Audio_Streaming">
-		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-05T18:40:40+00:00</swivt:creationDate>
+		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-05T23:18:18+00:00</swivt:creationDate>
 		<owl:imports rdf:resource="http://semantic-mediawiki.org/swivt/1.0"/>
 	</owl:Ontology>
 	<swivt:Subject rdf:about="http://love2d.org/wiki/Special:URIResolver/In-2DMemory_Audio_Streaming">
