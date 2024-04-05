469d36909fb1b023739bb4148bcb1fc54f9a3af3
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-04-05 18:03:34 -0500

    Automatic partial update At: Fri Apr  5 06:03:34 PM -05 2024

--- love2d.org/w/indexd277.php
+++ love2d.org/w/indexd277.php
@@ -20,7 +20,7 @@
 	xmlns:property="&property;">
 
 	<owl:Ontology rdf:about="http://love2d.org/wiki/Special:ExportRDF/Free_Game_Resources">
-		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-05T18:12:28+00:00</swivt:creationDate>
+		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-05T23:03:26+00:00</swivt:creationDate>
 		<owl:imports rdf:resource="http://semantic-mediawiki.org/swivt/1.0"/>
 	</owl:Ontology>
 	<swivt:Subject rdf:about="http://love2d.org/wiki/Special:URIResolver/Free_Game_Resources">
