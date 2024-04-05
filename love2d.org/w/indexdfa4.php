f002511f8ca7c161eca7aa088f8b2395942124b4
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-04-05 18:57:51 -0500

    Automatic partial update At: Fri Apr  5 06:57:51 PM -05 2024

--- love2d.org/w/indexdfa4.php
+++ love2d.org/w/indexdfa4.php
@@ -20,7 +20,7 @@
 	xmlns:property="&property;">
 
 	<owl:Ontology rdf:about="http://love2d.org/wiki/Special:ExportRDF/Category-3ATutorials_-28Tiếng_Việt-29">
-		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-05T21:28:21+00:00</swivt:creationDate>
+		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-05T23:57:37+00:00</swivt:creationDate>
 		<owl:imports rdf:resource="http://semantic-mediawiki.org/swivt/1.0"/>
 	</owl:Ontology>
 	<owl:Class rdf:about="http://love2d.org/wiki/Special:URIResolver/Category-3ATutorials_-28Tiếng_Việt-29">
