c3c654cb87607f70ef0f23dad3a2874170d655e0
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-04-05 17:06:59 -0500

    Automatic partial update At: Fri Apr  5 05:06:58 PM -05 2024

--- constant_push.sh
+++ constant_push.sh
@@ -1,3 +1,9 @@
+# This code constanly pushes the progress
+# of httrack to the repository
+# since automation in the repository itself is capped at 6hr max
+# this avoids corruption on power losses and such but also another
+# reason this script exist is quite petty, so my commit history gets
+# inflated more than i'll ever be able to do legit.
 MSG="Automatic partial update"
 COOLDOWN="1m"
 
