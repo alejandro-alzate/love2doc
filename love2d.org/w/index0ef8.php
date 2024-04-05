f35611259af61acbee95f93437424b4f34ed58b0
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-04-05 18:36:46 -0500

    Automatic partial update At: Fri Apr  5 06:36:46 PM -05 2024

--- love2d.org/w/index0ef8.php
+++ love2d.org/w/index0ef8.php
@@ -20,7 +20,7 @@
 	xmlns:property="&property;">
 
 	<owl:Ontology rdf:about="http://love2d.org/wiki/Special:ExportRDF/Tutorial-3AGraphic_Tranformations">
-		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-05T19:07:23+00:00</swivt:creationDate>
+		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-05T23:36:28+00:00</swivt:creationDate>
 		<owl:imports rdf:resource="http://semantic-mediawiki.org/swivt/1.0"/>
 	</owl:Ontology>
 	<swivt:Subject rdf:about="http://love2d.org/wiki/Special:URIResolver/Tutorial-3AGraphic_Tranformations">
