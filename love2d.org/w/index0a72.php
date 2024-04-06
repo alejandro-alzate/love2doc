fbb58f32457b5362d5e0f7086eef95b39254ab37
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-04-06 14:38:16 -0500

    Automatic partial update At: Sat Apr  6 02:38:16 PM -05 2024

--- love2d.org/w/index0a72.php
+++ love2d.org/w/index0a72.php
@@ -20,7 +20,7 @@
 	xmlns:property="&property;">
 
 	<owl:Ontology rdf:about="http://love2d.org/wiki/Special:ExportRDF/love.touchreleased">
-		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-06T16:53:56+00:00</swivt:creationDate>
+		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-06T19:33:18+00:00</swivt:creationDate>
 		<owl:imports rdf:resource="http://semantic-mediawiki.org/swivt/1.0"/>
 	</owl:Ontology>
 	<swivt:Subject rdf:about="http://love2d.org/wiki/Special:URIResolver/love.touchreleased">
