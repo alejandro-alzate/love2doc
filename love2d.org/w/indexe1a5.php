838661a8afc0a241c9a430537b266bedc8a29ce0
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-04-06 10:21:12 -0500

    Automatic partial update At: Sat Apr  6 10:21:11 AM -05 2024

--- love2d.org/w/indexe1a5.php
+++ love2d.org/w/indexe1a5.php
@@ -20,7 +20,7 @@
 	xmlns:property="&property;">
 
 	<owl:Ontology rdf:about="http://love2d.org/wiki/Special:ExportRDF/love.physics">
-		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-06T14:39:18+00:00</swivt:creationDate>
+		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-06T15:20:03+00:00</swivt:creationDate>
 		<owl:imports rdf:resource="http://semantic-mediawiki.org/swivt/1.0"/>
 	</owl:Ontology>
 	<swivt:Subject rdf:about="http://love2d.org/wiki/Special:URIResolver/love.physics">
