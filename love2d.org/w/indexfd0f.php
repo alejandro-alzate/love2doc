0fbef98d8ba38dd13b6e7c78e3059685caed57b4
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-04-06 14:30:59 -0500

    manual update

--- love2d.org/w/indexfd0f.php
+++ love2d.org/w/indexfd0f.php
@@ -20,7 +20,7 @@
 	xmlns:property="&property;">
 
 	<owl:Ontology rdf:about="http://love2d.org/wiki/Special:ExportRDF/love.joystickadded">
-		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-06T16:47:15+00:00</swivt:creationDate>
+		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-06T19:26:33+00:00</swivt:creationDate>
 		<owl:imports rdf:resource="http://semantic-mediawiki.org/swivt/1.0"/>
 	</owl:Ontology>
 	<swivt:Subject rdf:about="http://love2d.org/wiki/Special:URIResolver/love.joystickadded">
