7b792892a8877ea0634d2375c54164d415ae5a32
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-04-06 13:21:05 -0500

    Automatic partial update At: Sat Apr  6 01:21:05 PM -05 2024

--- love2d.org/w/index75d1.php
+++ love2d.org/w/index75d1.php
@@ -20,7 +20,7 @@
 	xmlns:property="&property;">
 
 	<owl:Ontology rdf:about="http://love2d.org/wiki/Special:ExportRDF/Getting_Started_-28Українська-29">
-		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-06T15:37:07+00:00</swivt:creationDate>
+		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-06T18:19:28+00:00</swivt:creationDate>
 		<owl:imports rdf:resource="http://semantic-mediawiki.org/swivt/1.0"/>
 	</owl:Ontology>
 	<swivt:Subject rdf:about="http://love2d.org/wiki/Special:URIResolver/Getting_Started_-28Українська-29">
