c4856741402f71e14ce87b9d4d22f60686dd55db
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-04-06 13:45:35 -0500

    Automatic partial update At: Sat Apr  6 01:45:35 PM -05 2024

--- love2d.org/w/index778b.php
+++ love2d.org/w/index778b.php
@@ -20,7 +20,7 @@
 	xmlns:property="&property;">
 
 	<owl:Ontology rdf:about="http://love2d.org/wiki/Special:ExportRDF/Tutorial-3AHamster_Ball_-28日本語-29">
-		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-06T16:03:10+00:00</swivt:creationDate>
+		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-06T18:45:08+00:00</swivt:creationDate>
 		<owl:imports rdf:resource="http://semantic-mediawiki.org/swivt/1.0"/>
 	</owl:Ontology>
 	<swivt:Subject rdf:about="http://love2d.org/wiki/Special:URIResolver/Tutorial-3AHamster_Ball_-28日本語-29">
