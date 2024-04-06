8f5706adb6d1f8d399a7b1cd164724813882ce30
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-04-06 13:54:00 -0500

    Found an easter egg archiving that as well Pretty hidden in plain sight tbh

--- love2d.org/w/indexb500.php
+++ love2d.org/w/indexb500.php
@@ -20,7 +20,7 @@
 	xmlns:property="&property;">
 
 	<owl:Ontology rdf:about="http://love2d.org/wiki/Special:ExportRDF/Tutorial-3AObject_Follows_Mouse">
-		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-06T16:11:36+00:00</swivt:creationDate>
+		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-06T18:52:24+00:00</swivt:creationDate>
 		<owl:imports rdf:resource="http://semantic-mediawiki.org/swivt/1.0"/>
 	</owl:Ontology>
 	<swivt:Subject rdf:about="http://love2d.org/wiki/Special:URIResolver/Tutorial-3AObject_Follows_Mouse">
