57c9ca475c22b3d6dc274fc99cbf0b47cf519daf
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-04-06 11:20:21 -0500

    Automatic partial update At: Sat Apr  6 11:20:21 AM -05 2024

---
+++ love2d.org/w/index3cef.php
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE rdf:RDF[
+	<!ENTITY rdf 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'>
+	<!ENTITY rdfs 'http://www.w3.org/2000/01/rdf-schema#'>
+	<!ENTITY owl 'http://www.w3.org/2002/07/owl#'>
+	<!ENTITY swivt 'http://semantic-mediawiki.org/swivt/1.0#'>
+	<!ENTITY wiki 'http://love2d.org/wiki/Special:URIResolver/'>
+	<!ENTITY category 'http://love2d.org/wiki/Special:URIResolver/Category-3A'>
+	<!ENTITY property 'http://love2d.org/wiki/Special:URIResolver/Property-3A'>
+	<!ENTITY wikiurl 'http://love2d.org/wiki/'>
+]>
+
+<rdf:RDF
+	xmlns:rdf="&rdf;"
+	xmlns:rdfs="&rdfs;"
+	xmlns:owl ="&owl;"
+	xmlns:swivt="&swivt;"
+	xmlns:wiki="&wiki;"
+	xmlns:category="&category;"
+	xmlns:property="&property;">
+
+	<owl:Ontology rdf:about="http://love2d.org/wiki/Special:ExportRDF/Category-3ATutorials_-28Indonesia-29">
+		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-06T16:20:13+00:00</swivt:creationDate>
+		<owl:imports rdf:resource="http://semantic-mediawiki.org/swivt/1.0"/>
+	</owl:Ontology>
+	<owl:Class rdf:about="http://love2d.org/wiki/Special:URIResolver/Category-3ATutorials_-28Indonesia-29">
+		<rdfs:label>Tutorials (Indonesia)</rdfs:label>
+		<rdfs:isDefinedBy rdf:resource="http://love2d.org/wiki/Special:ExportRDF/Category-3ATutorials_-28Indonesia-29"/>
+		<swivt:page rdf:resource="http://love2d.org/wiki/Category-3ATutorials_-28Indonesia-29"/>
+		<swivt:wikiNamespace rdf:datatype="http://www.w3.org/2001/XMLSchema#integer">14</swivt:wikiNamespace>
+		<swivt:wikiPageContentLanguage rdf:datatype="http://www.w3.org/2001/XMLSchema#string">en</swivt:wikiPageContentLanguage>
+		<property:Has_query rdf:resource="http://love2d.org/wiki/Special:URIResolver/Category-3ATutorials_-28Indonesia-29-23_QUERY9cf0f7e9f1d3ec5a3c44714002042bb8"/>
+		<swivt:wikiPageModificationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2020-01-08T06:04:29Z</swivt:wikiPageModificationDate>
+		<property:Modification_date-23aux rdf:datatype="http://www.w3.org/2001/XMLSchema#double">2458856.7531134</property:Modification_date-23aux>
+		<swivt:wikiPageSortKey rdf:datatype="http://www.w3.org/2001/XMLSchema#string">Tutorials (Indonesia)</swivt:wikiPageSortKey>
+		<rdfs:subClassOf rdf:resource="http://love2d.org/wiki/Special:URIResolver/Category-3ALÖVE_-28Indonesia-29"/>
+	</owl:Class>
+	<swivt:Subject rdf:about="http://love2d.org/wiki/Special:URIResolver/Category-3ATutorials_-28Indonesia-29-23_QUERY9cf0f7e9f1d3ec5a3c44714002042bb8">
+		<swivt:masterPage rdf:resource="http://love2d.org/wiki/Special:URIResolver/Category-3ATutorials_-28Indonesia-29"/>
+		<swivt:wikiNamespace rdf:datatype="http://www.w3.org/2001/XMLSchema#integer">14</swivt:wikiNamespace>
+		<property:Query_depth rdf:datatype="http://www.w3.org/2001/XMLSchema#double">0</property:Query_depth>
+		<property:Query_format rdf:datatype="http://www.w3.org/2001/XMLSchema#string">table</property:Query_format>
+		<property:Query_size rdf:datatype="http://www.w3.org/2001/XMLSchema#double">1</property:Query_size>
+		<property:Query_string rdf:datatype="http://www.w3.org/2001/XMLSchema#string">[[Category:Tutorials (Indonesia)]]</property:Query_string>
+		<swivt:wikiPageSortKey rdf:datatype="http://www.w3.org/2001/XMLSchema#string">Tutorials (Indonesia)# QUERY9cf0f7e9f1d3ec5a3c44714002042bb8</swivt:wikiPageSortKey>
+	</swivt:Subject>
+	<swivt:Subject rdf:about="http://love2d.org/wiki/Special:URIResolver/Tutorials_-28Indonesia-29">
+		<rdfs:label>Tutorials (Indonesia)</rdfs:label>
+		<rdfs:isDefinedBy rdf:resource="http://love2d.org/wiki/Special:ExportRDF/Tutorials_-28Indonesia-29"/>
+		<swivt:page rdf:resource="http://love2d.org/wiki/Tutorials_-28Indonesia-29"/>
+		<swivt:wikiNamespace rdf:datatype="http://www.w3.org/2001/XMLSchema#integer">0</swivt:wikiNamespace>
+		<swivt:wikiPageContentLanguage rdf:datatype="http://www.w3.org/2001/XMLSchema#string">en</swivt:wikiPageContentLanguage>
+		<swivt:redirectsTo rdf:resource="http://love2d.org/wiki/Special:URIResolver/Category-3ATutorials_-28Indonesia-29"/>
+		<owl:sameAs rdf:resource="http://love2d.org/wiki/Special:URIResolver/Category-3ATu
