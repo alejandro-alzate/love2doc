c2c8262e7671bab2877d9e1e93740e9c8ecf4339
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-04-06 09:06:48 -0500

    Automatic partial update At: Sat Apr  6 09:06:48 AM -05 2024

--- love2d.org/w/index1707.php
+++ love2d.org/w/index1707.php
@@ -20,7 +20,7 @@
 	xmlns:property="&property;">
 
 	<owl:Ontology rdf:about="http://love2d.org/wiki/Special:ExportRDF/Game_Distribution">
-		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-06T02:14:31+00:00</swivt:creationDate>
+		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-06T14:04:32+00:00</swivt:creationDate>
 		<owl:imports rdf:resource="http://semantic-mediawiki.org/swivt/1.0"/>
 	</owl:Ontology>
 	<swivt:Subject rdf:about="http://love2d.org/wiki/Special:URIResolver/Game_Distribution">
