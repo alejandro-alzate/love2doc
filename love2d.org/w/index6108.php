a0787836e6992072d6c3f45eb2ea58f0345c394b
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-04-05 20:39:03 -0500

    Automatic partial update At: Fri Apr  5 08:39:03 PM -05 2024

--- love2d.org/w/index6108.php
+++ love2d.org/w/index6108.php
@@ -20,7 +20,7 @@
 	xmlns:property="&property;">
 
 	<owl:Ontology rdf:about="http://love2d.org/wiki/Special:ExportRDF/love.font">
-		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-05T22:41:19+00:00</swivt:creationDate>
+		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-06T01:38:40+00:00</swivt:creationDate>
 		<owl:imports rdf:resource="http://semantic-mediawiki.org/swivt/1.0"/>
 	</owl:Ontology>
 	<swivt:Subject rdf:about="http://love2d.org/wiki/Special:URIResolver/love.font">
