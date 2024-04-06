c99fe6cc6fa7570ad25b64e8372977f03b19dad8
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-04-05 19:14:09 -0500

    Automatic partial update At: Fri Apr  5 07:14:09 PM -05 2024

--- love2d.org/w/index8c13.php
+++ love2d.org/w/index8c13.php
@@ -20,7 +20,7 @@
 	xmlns:property="&property;">
 
 	<owl:Ontology rdf:about="http://love2d.org/wiki/Special:ExportRDF/11.3">
-		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-05T21:52:05+00:00</swivt:creationDate>
+		<swivt:creationDate rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">2024-04-06T00:14:00+00:00</swivt:creationDate>
 		<owl:imports rdf:resource="http://semantic-mediawiki.org/swivt/1.0"/>
 	</owl:Ontology>
 	<swivt:Subject rdf:about="http://love2d.org/wiki/Special:URIResolver/11.3">
